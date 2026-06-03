0cc8eadd8189d468ea7d483a748bede9228b3adf
dyx <12595538+dyx@users.noreply.github.com>
2026-06-03 05:24:29 -0700

    refactor: unregister clip_summary tool from agent

--- electron/agent/tools/index.ts
+++ electron/agent/tools/index.ts
@@ -1,10 +1,8 @@
 import { searchFilesTool } from './search_files'
-import { clipSummaryTool } from './clip_summary'
 import { getGroveStatsTool } from './get_grove_stats'
 
 export const agentTools = [
   searchFilesTool,
-  clipSummaryTool,
   getGroveStatsTool
 ] as const
 
