67c71f59aa6f33a4f90204c954188da0c342890b
dyx <12595538+dyx@users.noreply.github.com>
2026-06-06 08:05:35 -0700

    chore: remove dead code and migrate font-review class to AiReviewSidebar

--- src/components/editor/useAiDialog.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react'
-import { AiReviewDialogContainer } from './AiReviewDialogContainer'
-
-export function useAiDialog(clipId: number | null) {
-  const [open, setOpen] = useState(false)
-  const dialog = <AiReviewDialogContainer open={open} clipId={clipId} onClose={() => setOpen(false)} />
-  return { dialog, openDialog: () => setOpen(true) }
-}
